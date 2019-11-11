f0788e0ed9e8c4dfae1a049afc75a460fac04140
EthanQuist <ethantquist@gmail.com>
2019-11-10 18:03:47 -0800

    Added Bagfile functionality added to launch and tested listener

--- src/talker.cpp
+++ src/talker.cpp
@@ -149,16 +149,19 @@ int main(int argc, char **argv) {
      * in the constructor above.
      */
     chatter_pub.publish(msg);
-    tf::TransformBroadcaster br;
-    tf::Transform transform;
-    ros::Rate rate(10.0);
-    while (n.ok()) {
-      transform.setOrigin(tf::Vector3(0.0, 2.0, 0.0));
-      transform.setRotation(tf::Quaternion(0, 0, 0, 1));
-      br.sendTransform(
-          tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
+
+
+
+    //tf::TransformBroadcaster br;
+    //tf::Transform transform;
+    //ros::Rate rate(10.0);
+    //while (n.ok()) {
+    //  transform.setOrigin(tf::Vector3(0.0, 2.0, 0.0));
+    //  transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+    //  br.sendTransform(
+    //      tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
       //  rate.sleep();
-    }
+    //}
 
     ros::spinOnce();
 
